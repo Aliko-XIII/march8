92a04fa2c40cae35bccc1bf5ba9cf05fd9e638ed
Aliko Tektov <tektov2004@gmail.com>
2024-03-07 09:33:52 +0200

    empty app create react is shit

---
+++ src/components/App.tsx
@@ -0,0 +1,12 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { SetStateAction, createContext, useState } from 'react';
+
+
+
+const App = () => {
+    return (<>
+    </>);
+}
+
+
+export default App;
